c1f7511cdab60876e6b86b75aa7608205c5ad94e
ChristianOtieno <cgao273@gmail.com>
2019-12-12 22:25:51 +0300

    Add user's posts validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 80 }
+  validates :content, presence: true, length: { maximum: 8000 }
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,33 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = User.new(title: 'Title Example', content: 'This is a content example')
+  end
+
+  test 'should be valid' do
+    assert @post.valid?
+  end
+
+  test 'title should be present' do
+    @post.title = '    '
+    assert_not @post.valid?
+  end
+
+  test 'content should be present' do
+    @post.content = '    '
+    assert_not @post.valid?
+  end
+
+  test 'title should not be too long' do
+    @post.title = 'a' * 80
+    assert_not @post.valid?
+  end
+
+  test 'content should not be too long' do
+    @post.content = 'a' * 8000
+    assert_not @post.valid?
+  end
 end
