48e879d50725ef692e2774b962db19454bebb265
ChristianOtieno <cgao273@gmail.com>
2019-12-13 00:42:10 +0300

    ADD database migration changes to suit user_id key

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_191208) do
+ActiveRecord::Schema.define(version: 2019_12_12_213329) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
