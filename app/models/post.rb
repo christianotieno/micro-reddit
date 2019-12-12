fcd2f278e5cb3880a365d08e5dc9b3babe4d2e48
ChristianOtieno <cgao273@gmail.com>
2019-12-13 00:15:01 +0300

    Add user's and posts' association rules

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 80 }
   validates :content, presence: true, length: { maximum: 8000 }
+
+  belongs_to :user, class_name: "user", foreign_key: "user_id"
 end
