810a56067b36a540886a41d56334295a381e274b
ChristianOtieno <cgao273@gmail.com>
2019-12-13 19:43:14 +0300

    ADD comments validations rules

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = Comment.new(body: 'Title Example', user_id:1, post_id:1)
+  end
+
+  test 'should be valid' do
+    assert @comment.valid?
+  end
+
+  test 'body should be present' do
+    @post.body = '    '
+    assert_not @comment.valid?
+  end
+
+  test 'user id should be present' do
+    @post.body = '    '
+    assert_not @comment.valid?
+  end
+
+  test 'post id should be present' do
+    @post.body = '    '
+    assert_not @comment.valid?
+  end
 end
